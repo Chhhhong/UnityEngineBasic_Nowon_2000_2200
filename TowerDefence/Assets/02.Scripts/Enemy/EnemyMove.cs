08fe1e34a80e72687faf65ad37216ab6ec5f30ac
koseyun <a36324606@gmail.com>
2022-10-06 21:51:35 +0900

    Unity TowerDefence Making RocketLauncer
    Unity TowerDefence Making RocketLauncer

--- TowerDefence/Assets/02.Scripts/Enemy/EnemyMove.cs
+++ TowerDefence/Assets/02.Scripts/Enemy/EnemyMove.cs
@@ -7,6 +7,7 @@ public class EnemyMove : MonoBehaviour
     private Transform _tr;
     private Enemy _enemy;
     public float speed = 1.0f;
+    [SerializeField] private float _offsetY;
 
     private Pathfinder _pathFinder;
     [SerializeField]private Transform _start;
@@ -32,7 +33,6 @@ public class EnemyMove : MonoBehaviour
         _tr = GetComponent<Transform>();
         _enemy = GetComponent<Enemy>();
         _pathFinder = GetComponent<Pathfinder>();
-        _originY = _tr.position.y;
     }
 
     private void Start()
@@ -43,6 +43,7 @@ public class EnemyMove : MonoBehaviour
         }
 
         _nextWayPoint = _wayPoints[0];
+        _originY = _tr.position.y + _offsetY;
     }
 
     private void FixedUpdate()
